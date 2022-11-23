54a4c95327a81fb42c77331a15581ceb2a7920e7
Vitaly <itassan6@gmail.com>
2022-11-23 13:48:20 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 import { setupStore } from "./store/store";
 
@@ -12,8 +13,8 @@ const store = setupStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </Provider>
 );
