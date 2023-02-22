7228b50569c85f3382b562f45ceb94fab19f7321
Vitaly <itassan6@gmail.com>
2023-02-22 14:24:05 +0200

    Fixed burger

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,14 +1,12 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
-  const ref = useRef();
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   return {
-    ref,
     value,
     setValue,
     onChange: handleChange,
